54bc33a608e907725d06eb6721a4869aa5c7d834
Luke Merrett <luke.merrett@just-eat.com>
2020-11-11 14:43:40 +0000

    Bootstrapped project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_9_Record_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
